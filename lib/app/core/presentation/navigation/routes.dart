494d5b9560d6a9d0914352339443aa650a5ea13d
Victor T Costa <avictortcosta@gmail.com>
2023-11-06 00:04:36 -0300

    Added main, app_module and core components

---
+++ lib/app/core/presentation/navigation/routes.dart
@@ -0,0 +1,9 @@
+abstract class Routes {
+  static const root = Paths.root;
+  static const map = Paths.map;
+}
+
+abstract class Paths {
+  static const root = '/';
+  static const map = '/map/';
+}
